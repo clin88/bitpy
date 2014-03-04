63085c67d7d917a6e1c07c3d8c5480a60538382a
Chen Lin <clin_@berkeley.edu>
2014-03-04 17:11:28 -0500

    Downloaded peer torrent.

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
+from itertools import chain
 import bencode
 import requests
+from requests import RequestException
 import hashlib
 
-def getPeerList(metadata):
-    url = 'announce-url?'
-    info_hash = hashlib.sha1(bencode.bencode(metadata['info'])))
-    info_hash_param = 'param={0}&'.format(info_hash.digest())
-    peer_id
-    port
-    uploaded
-    downloaded
-    left
-    compact
-    no_peer_id
-    event
+def get_tracker_response(url, metainfo):
+    params = {
+        "info_hash": hashlib.sha1(bencode.bencode(metainfo['info'])).digest(),
+        "peer_id": "JEFF-123456789012356",
+        "left": get_left(metainfo),
+    }
+    print url
+    return bencode.bdecode(requests.get(url, params=params).content)
+
+def get_left(metainfo):
+    sum = 0
+    for file in metainfo['info']['files']:
+        sum += file['length']
+
+    return sum
+
+def trackerlist(metainfo):
+    yield metainfo['announce']
+    for tracker in chain(*metainfo['announce-list']):
+        yield tracker
+
+def getPeerAddr(response):
+    decoded_dec_peer = map(ord, response['peers'])
+
 
 def main():
-    torrent = open(filename, 'r')
-    metadata = bencode.bdecode(torrent)
-    peer_list = getPeerList(metadata)
-    lexer = TorrentLexer(torrent_input)
-    info_hash
+    with open("test2.torrent", 'rb') as inpf:
+        metainfo = bencode.bdecode(inpf.read())
+
+    for tracker in trackerlist(metainfo):
+        try:
+            response = get_tracker_response(tracker, metainfo)
+            print response
+        except RequestException, e:
+            continue
+
+    #addr_list = getPeerAddr(response)
+
+if __name__ == "__main__":
+    main()
